f358aabdc97a6eb6590238ffd4114e135a7c59fd
Samuel <samuel1998.schwanzer@gmail.com>
2022-10-03 22:18:40 +0200

    Added livsi logos.

--- src/login/LoginPage.tsx
+++ src/login/LoginPage.tsx
@@ -5,7 +5,7 @@ import {useAppDispatch} from "../store/hooks";
 import {serverurl} from "../index";
 import {useState} from "react";
 import {setAccessToken, setExpiresIn, setRefreshToken} from "./loginSlice";
-
+import logo from './LIVISI_MARK_10.png'
 
 export const LoginPage = () => {
     const dispatch = useAppDispatch()
@@ -43,12 +43,7 @@ export const LoginPage = () => {
     return (
         <div className="w-screen h-screen flex justify-center items-center bg-gray-100" id="login">
             <form className="p-10 bg-white rounded flex justify-center items-center flex-col shadow-md">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5}
-                     stroke="currentColor" className="w-20 h-20">
-                    <path strokeLinecap="round" strokeLinejoin="round"
-                          d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963
-                           0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z" />
-                </svg>
+                <img src={logo} alt="Livsi Logo" className="w-20 h-20"/>
 
                 <p className="mb-5 text-3xl uppercase text-gray-600">Login</p>
                 <input name="email"
