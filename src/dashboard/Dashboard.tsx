9f4877cc871aca4f7fef2b47cd52a173976df416
samuel1998.schwanzer <samuel1998.schwanzer@gmail.com>
2022-10-10 13:54:40 +0200

    Fixed sidebar.

--- src/dashboard/Dashboard.tsx
+++ src/dashboard/Dashboard.tsx
@@ -10,7 +10,7 @@ export const Dashboard = ()=>{
         <div className="grid grid-cols-1 p-6 gap-y-20 gap-x-2 h-full w-full">
             {locations.map(location=><div key={location.id}>
                 <h2 className="text-4xl mb-5">{location.config.name}</h2>
-                <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-2 gap-y-3 text-white">
+                <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-y-3 gap-x-3 text-white">
                 {devices.filter(device=>device.location && device.location.includes(location.id) && device.config.protocolId!=='Virtual').map(deviceInLocation=>
                     <div key={deviceInLocation.id}
                         className={"block p-6 max-w-sm rounded-lg border border-gray-200 shadow-md hover:bg-gray-100 bg-gray-800 border-gray-700 hover:bg-gray-700"}>
--- src/sidebar/SideBar.tsx
+++ src/sidebar/SideBar.tsx
@@ -14,7 +14,7 @@ export const SideBar  = ()=>{
         return ''
     }
 
-    return <aside className={`w-full h-full float-left ${sideBarCollapsed?'hidden': 'col-span-6 md:col-span-1'} z-10 flex`} aria-label="Sidebar">
+    return <aside className={`w-full h-full float-left ${sideBarCollapsed?'hidden': 'col-span-6 md:col-span-1'} z-10 flex md:w-60`} aria-label="Sidebar">
         <div className="py-4 px-3 bg-gray-800 h-full w-full">
             <ul className="space-y-2">
                 <li>
@@ -32,7 +32,7 @@ export const SideBar  = ()=>{
                 </li>
                 <li>
                     <a onClick={()=>navigate('devices')}
-                       className={`flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 ${highlightIfSelected("/devices")}`}>
+                       className={`flex items-center p-2 text-base font-normal rounded-lg text-white hover:bg-gray-700 ${highlightIfSelected("/devices")}`}>
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5"
                              stroke="currentColor" className="w-6 h-6">
                             <path strokeLinecap="round" strokeLinejoin="round"
@@ -82,7 +82,7 @@ export const SideBar  = ()=>{
                 </li>
                 <li>
                     <a onClick={()=>navigate("/accounts")}
-                       className="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
+                       className="flex items-center p-2 text-base font-normal rounded-lg text-white hover:bg-gray-700">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                             <path strokeLinecap="round" strokeLinejoin="round" d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z" />
                         </svg>
