a29ff8fa41e15b0ff95964f0760a99f7f6ac388e
SamTV12345 <40429738+samtv12345@users.noreply.github.com>
2024-01-31 10:43:58 +0100

    Fixed 404 page redirect.

--- ui-new/src/pages/404Page.tsx
+++ ui-new/src/pages/404Page.tsx
@@ -1,10 +1,13 @@
+import {useNavigate} from "react-router-dom";
+
 export const Page404 = () => {
+    const navigate = useNavigate()
     return (
         <div className="flex flex-col items-center justify-center h-screen">
             <div className="flex flex-col items-center justify-center bg-white pl-5 pr-5 pt-2 pb-2 rounded-xl">
                 <h1 className="text-8xl font-bold">404</h1>
                 <p className="text-gray-500">Seite nicht gefunden</p>
-                <a href="/" className="text-blue-500">Zurück zur Startseite</a>
+                <a onClick={()=>navigate('/')} className="text-blue-500">Zurück zur Startseite</a>
             </div>
         </div>
     )
