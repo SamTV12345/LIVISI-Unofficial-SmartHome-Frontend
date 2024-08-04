ab94d97459ca628cd75a51c3edb378c80951bed2
SamTV12345 <40429738+samtv12345@users.noreply.github.com>
2024-08-04 20:55:35 +0200

    Add bat script to generated windows file

---
+++ run.bat
@@ -0,0 +1,19 @@
+@echo off
+
+REM Reminder: If your password contains a percentage sign, escape it with another percentage sign
+
+set BASE_URL=<your-base-url-to-livisi-shc>
+set PASSWORD=<your-password>
+
+
+REM set BASIC_USERNAME=login-username
+REM set BASIC_PASSWORD=login-password
+REM set BASIC_AUTH=true
+
+REM set OIDC_CLIENT_ID=your-oidc-client-id
+REM set OIDC_AUTH=true
+REM set OIDC_REDIRECT_URI=your-oidc-redirect-uri
+REM set OIDC_SCOPE=your-oidc-scope
+REM set OIDC_AUTHORITY=your-oidc-authority
+
+api.exe
